be0eab1fad1bb9f4dc78f81b3b7b925d377da231
soohyeon <meltapplee@gmail.com>
2025-05-09 16:18:29 +0900

    chore :: assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
-function App() {
-  return <></>;
-}
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Jobs from "./pages/Job/Jobs";
+import JobDetail from "./pages/Job/JobDetail";
+import Community from "./pages/Community";
+import CommunityPostDetail from "./pages/CommunityPostDetail";
+import CommunityWrite from "./pages/CommunityWrite";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/:id" element={<JobDetail />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/community/:id" element={<CommunityPostDetail />} />
+          <Route path="/community/write" element={<CommunityWrite />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
 
 export default App;
