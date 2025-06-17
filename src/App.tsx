6fce08861b46bc68967ffbcf1060529aa03c00d2
soohyeon <meltapplee@gmail.com>
2025-06-17 11:44:56 +0900

    chore :: 버전 수정 등 자잘한 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Jobs from "./pages/Job/Jobs";
 import JobDetail from "./pages/Job/JobDetail";
@@ -14,26 +14,30 @@ import NotFound from "./pages/NotFound";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 
+import { AuthProvider } from "@/lib/AuthContext";
+
 const queryClient = new QueryClient();
 
 const App = () => (
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/jobs/:id" element={<JobDetail />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/community/:id" element={<CommunityPostDetail />} />
-          <Route path="/community/write" element={<CommunityWrite />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </TooltipProvider>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/jobs/:id" element={<JobDetail />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/community/:id" element={<CommunityPostDetail />} />
+            <Route path="/community/write" element={<CommunityWrite />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </TooltipProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </BrowserRouter>
 );
