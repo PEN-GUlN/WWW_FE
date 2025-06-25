f0c106afe38dbf224da96d6d5cc70fd9aad48d79
soohyeon <meltapplee@gmail.com>
2025-06-25 11:48:16 +0900

    chore :: 북마크 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,5 @@ export { default as EyeOff } from "./Eyeoff.svg";
 export { default as EyeOpen } from "./EyeOpen.svg";
 export { default as Logo } from "./Logo.svg";
 export { default as Default } from "./Default.svg";
+export { default as Bookmark } from "./Bookmark.svg";
+export { default as Bookmarked } from "./Bookmarked.svg";
